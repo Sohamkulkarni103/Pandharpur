d816357fc4807e623a4bfa557ba45bcdd6c6f655
srk <soham.kulkarni@cyberrafting.com>
2024-08-06 14:01:21 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,35 +1,128 @@
 
+import { useState } from 'react';
 import './App.css';
-import Home from './Components/Home';
 
-import { Fragment } from "react";
-
-import About from './routes/About';
-import Vitthal from './routes/Vitthal';
-import Life from './routes/Life';
-import { Route, Routes } from "react-router-dom";
-import Education from './routes/Education';
-import Food from './routes/Food';
-import { Helmet } from "react-helmet";
 function App() {
+   const [value, setValue] = useState("");
   return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/vitthal" element={<Vitthal />} />
-        <Route path="/life" element={<Life />} />
-        <Route path="/education" element={<Education />} />
-        <Route path="/food" element={<Food />} />
-      </Routes>
-      
+    <div className="container">
+      <div className="calculator">
+        <form action="">
+          <div className="display">
+            <input type="text" value={value} />
+          </div>
+          <div>
+            <input type="button" value="AC" onClick={(e) => setValue("")} />
+            <input
+              type="button"
+              value="DE"
+              onClick={(e) => setValue(value.slice(0, -1))}
+            />
+            <input
+              type="button"
+              value="."
+              onClick={(e) => setValue(value + e.target.value)}
+            />
+            <input
+              type="button"
+              value="/"
+              onClick={(e) => setValue(value + e.target.value)}
+            />
+          </div>
+          <div>
+            <input
+              type="button"
+              value="7"
+              onClick={(e) => setValue(value + e.target.value)}
+            />
+            <input
+              type="button"
+              value="8"
+              onClick={(e) => setValue(value + e.target.value)}
+            />
+            <input
+              type="button"
+              value="9"
+              onClick={(e) => setValue(value + e.target.value)}
+            />
+            <input
+              type="button"
+              value="*"
+              onClick={(e) => setValue(value + e.target.value)}
+            />
+          </div>
+          <div>
+            <input
+              type="button"
+              value="4"
+              onClick={(e) => setValue(value + e.target.value)}
+            />
+            <input
+              type="button"
+              value="5"
+              onClick={(e) => setValue(value + e.target.value)}
+            />
+            <input
+              type="button"
+              value="6"
+              onClick={(e) => setValue(value + e.target.value)}
+            />
+            <input
+              type="button"
+              value="+"
+              onClick={(e) => setValue(value + e.target.value)}
+            />
+          </div>
+          <div>
+            <input
+              type="button"
+              value="1"
+              onClick={(e) => setValue(value + e.target.value)}
+            />
+            <input
+              type="button"
+              value="2"
+              onClick={(e) => setValue(value + e.target.value)}
+            />
+            <input
+              type="button"
+              value="3"
+              onClick={(e) => setValue(value + e.target.value)}
+            />
+            <input
+              type="button"
+              value="-"
+              onClick={(e) => setValue(value + e.target.value)}
+            />
+          </div>
+          <div>
+            <input
+              type="button"
+              value="00"
+              onClick={(e) => setValue(value + e.target.value)}
+            />
+            <input
+              type="button"
+              value="0"
+              onClick={(e) => setValue(value + e.target.value)}
+            />
+            <input
+              type="button"
+              value="="
+              className="equal"
+              onClick={(e) => setValue(eval(value))}
+            />
+            <input
+              type="button"
+              value="="
+              className="equal"
+              onClick={(e) => setValue(eval(value))}
+            />
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
 
 export default App;
-
-
-
-		
-	
--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.Fragment>
-    <BrowserRouter>
+  <React.StrictMode>
     <App />
-    </BrowserRouter>
-  </React.Fragment>
+  </React.StrictMode>
 );
 
 
